de56613e5f620611c9e3b1540e1d44e6eaccdc5c
Stefan <trfstephan@gmail.com>
2025-11-18 09:37:17 +0100

    Eliminar archivos innecesarios y corregir el nombre del proyecto a Megatronix

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
         Stefan Trifan
 
     *   Nombre del Proyecto
-        Megatronic
+        Megatronix
 */
 
 /* _________________________________________
@@ -13,7 +13,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TAM_LINEA 0
+#define NUM_FILAS 0
+
+// Variables Globales
+typedef struct 
+{
+    unsigned char ETQ;
+    unsigned char Data[TAM_LINEA];
+} 
+T_CACHE_LINE;
+
 // Funciones del programa
+void limpiar_cache(T_CACHE_LINE tbl[NUM_FILAS]);
+void volcar_cache(T_CACHE_LINE *tbl);
+void parsear_direccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque);
+void tratar_fallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque);
 
 // Funciones auxiliares
 void clearBuffer();
@@ -36,6 +51,46 @@ int main(int argc, char *argv[])
    Inicio definicion de funciones */
 
 // Funciones del programa
+/**
+ * @param[in] tbl   texto
+ */
+void limpiar_cache(T_CACHE_LINE tbl[NUM_FILAS])
+{
+
+}
+
+/**
+ * @param[out] tbl  texto
+ */
+void volcar_cache(T_CACHE_LINE *tbl)
+{
+
+}
+
+/**
+ * @param[in] addr      texto   
+ * @param[out] ETQ      texto    
+ * @param[out] palabra  texto   
+ * @param[out] linea    texto   
+ * @param[out] bloque   texto 
+ */
+void parsear_direccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque)
+{
+
+}
+
+/**
+ * @param[out] tbl      texto 
+ * @param[out] MRAM     texto
+ * @param[in] ETQ       texto
+ * @param[in] linea     texto
+ * @param[in] bloque    texto   
+ */
+void tratar_fallo(T_CACHE_LINE *tbl, char *MRAM, int ETQ, int linea, int bloque)
+{
+
+}
+
 
 // Funciones auxiliares
 void clearBuffer()
