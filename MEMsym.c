2a185ea0fc7487cda8155639aa759c4e07f5e8e6
ElAguacateSinPep <guguimunoz@gmail.com>
2025-11-30 18:33:34 +0100

    creacion de funcion parsear direccion

--- MEMsym.c
+++ MEMsym.c
@@ -33,3 +33,23 @@ void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]) {
 		for (int j = 0; j < TAM_LINEA; ++j) tbl[i].Data[j] = 0x23;
 	}
 }
+
+
+
+void ParsearDireccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque)
+{
+    // Aseguramos que la dirección está en 12 bits
+    addr &= 4095; // 0x0FFF en decimal
+
+    // palabra: últimos 4 bits (bits 0-3)
+    *palabra = addr & 15; // 15 = 0b1111 (4 bits encendidos)
+
+    // línea: siguientes 3 bits (bits 4-6)
+    *linea = (addr >> 4) & 7; // 7 = 0b111 (3 bits encendidos)
+
+    // etiqueta: siguientes 5 bits (bits 7-11)
+    *ETQ = (addr >> 7) & 31; // 31 = 0b11111 (5 bits encendidos)
+
+    // bloque: dirección base del bloque (múltiplo de 16)
+    *bloque = addr & (~15); // ~15 = todos menos los 4 bits menos significativos
+}
