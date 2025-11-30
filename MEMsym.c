242880cb312d3ec561ad17c41b1898e550e08f01
ElAguacateSinPep <guguimunoz@gmail.com>
2025-11-30 18:58:05 +0100

    se agrega el main

--- MEMsym.c
+++ MEMsym.c
@@ -9,7 +9,7 @@
 
 #define TAM_LINEA 16
 #define NUM_FILAS 8
-#define MEM_SIZE 4096
+#define TAM_RAM 4096
 
 typedef struct {
 	unsigned char ETQ;
@@ -25,6 +25,59 @@ void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]);
 void VolcarCACHE(T_CACHE_LINE *tbl);
 void ParsearDireccion(unsigned int addr, int *ETQ, int *palabra, int *linea, int *bloque);
 void TratarFallo(T_CACHE_LINE *tbl, unsigned char *MRAM, int ETQ, int linea, int bloque);
+
+int main() {
+    T_CACHE_LINE CACHE[NUM_FILAS];
+    unsigned char MRAM[TAM_RAM];
+    char texto[100] = {0};
+    int chars = 0;
+
+    LimpiarCACHE(CACHE);
+
+    // Leer RAM
+    FILE *f = fopen("CONTENTS_RAM.bin", "rb");
+    if (!f) {
+        printf("Error abriendo CONTENTS_RAM.bin\n");
+        return -1;
+    }
+    fread(MRAM, 1, TAM_RAM, f);
+    fclose(f);
+
+    // Leer direcciones
+    FILE *fd = fopen("dirs_memoria.txt", "r");
+    if (!fd) {
+        printf("Error abriendo dirs_memoria.txt\n");
+        return -1;
+    }
+
+    unsigned int addr;
+    while (fscanf(fd, "%x", &addr) == 1) {
+        int palabra, linea, ETQ, bloque;
+        ParsearDireccion(addr, &ETQ, &palabra, &linea, &bloque);
+
+        if (CACHE[linea].ETQ != ETQ) {
+            ++numfallos;
+            globaltime += 20;
+            TratarFallo(CACHE, MRAM, ETQ, linea, bloque);
+        } else {
+            ++globaltime;
+        }
+
+        texto[chars++] = CACHE[linea].Data[palabra];
+        texto[chars] = '\0';
+
+        imprimirContenidoCache(CACHE);
+        sleep(1);
+    }
+
+    fclose(fd);
+
+    printf("Texto leído: %s\n", texto);
+    printf("Fallos: %d, Tiempo total: %d\n", numfallos, globaltime);
+
+    VolcarCACHE(CACHE);
+    return 0;
+}
 
 void LimpiarCACHE(T_CACHE_LINE tbl[NUM_FILAS]) {
 	for (int i = 0; i < NUM_FILAS; ++i) {
@@ -81,7 +134,7 @@ void imprimirContenidoCache(T_CACHE_LINE tbl[NUM_FILAS]) {
 
     for (int i = 0; i < NUM_FILAS; ++i) {
         printf("Linea %X ETQ %X: ", i, tbl[i].ETQ);
-        
+
         for (int j = TAM_LINEA - 1; j >= 0; --j)
             printf("%02X ", tbl[i].Data[j]);
         printf("\n");
